1cec545b646b329231d04315570e7fcf65bfedfe
dodody <dody@bigpearl.io>
2020-07-25 20:43:00 +0900

    updated dody's data

--- gatsby-starter-lumen/config.js
+++ gatsby-starter-lumen/config.js
@@ -3,8 +3,8 @@
 module.exports = {
   url: 'https://lumen.netlify.com',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  title: '도디의 기술블로그',
+  subtitle: 'react와 react native로 개발하는걸 사랑하고, UXUI 디자인이 취미인 도딥니다 (๑˃̵ᴗ˂̵)و',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
@@ -12,32 +12,48 @@ module.exports = {
   useKatex: false,
   menu: [
     {
-      label: 'Articles',
-      path: '/'
+      label: '안녕하세용!',
+      path: '/pages/aboutdody'
     },
     {
-      label: 'About me',
-      path: '/pages/about'
+      label: 'React',
+      path: '/pages/react'
     },
     {
-      label: 'Contact me',
-      path: '/pages/contacts'
+      label: 'React Native',
+      path: '/pages/rn'
+    },
+    {
+      label: 'RN 라이브러리',
+      path: '/pages/rnlibrary'
+    },
+    {
+      label: 'React 라이브러리',
+      path: '/pages/reactlibrary'
+    },
+    {
+      label: '에러노트',
+      path: '/pages/errornote'
+    },
+    {
+      label: '개발기',
+      path: '/pages/developerstory'
     }
   ],
   author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    name: '개발자 도디',
+    photo: '/dody.jpg',
+    bio: 'react와 react native로 개발하는걸 사랑하고, UXUI 디자인이 취미인 도딥니다 (๑˃̵ᴗ˂̵)و',
     contacts: {
-      email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
+      email: 'dodyveloper@gmail.com',
+      facebook: '',
+      telegram: '',
+      twitter: '',
+      github: 'https://github.com/dodody',
       rss: '',
       vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
+      linkedin: '',
+      instagram: '',
       line: '',
       gitlab: '',
       weibo: '',
--- gatsby-starter-lumen/content/pages/aboutdody.md
+++ gatsby-starter-lumen/content/pages/aboutdody.md
@@ -1,10 +1,10 @@
 ---
-title: "About me"
+title: "안녕하세용!"
 template: "page"
 socialImage: "/media/image-2.jpg"
 ---
 
-Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.
+안녕하세요 도디예용!
 
 ![Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.](/media/image-2.jpg)
 
